aa0d7289ca63ab2f0f5f52c07688404b8f7fe6cf
Samuel Stachelski <samuel.vonstachelski@fhnw.ch>
2014-10-07 16:40:01 +0200

    Refactoring 2
    This is what follow the last commit ;)

--- EtherGL/src/ch/fhnw/ether/reorg/api/ILight.java
+++
@@ -1,7 +0,0 @@
-package ch.fhnw.ether.reorg.api;
-
-import ch.fhnw.ether.render.attribute.IUniformAttributeProvider;
-
-public interface ILight extends I3DObject, IUniformAttributeProvider{
-
-}
--- EtherGL/src/ch/fhnw/ether/reorg/api/IMesh.java
+++
@@ -1,9 +0,0 @@
-package ch.fhnw.ether.reorg.api;
-
-public interface IMesh extends I3DObject {
-	
-	IGeometry getGeometry();
-	
-	IMaterial getMaterial();
-	
-}
