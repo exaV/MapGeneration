65e39c5830160deca5f27fa32932d361c3652f3b
Simon Schubiger <sschubiger@bluewin.ch>
2015-10-02 15:41:30 +0200

    Fixed CustomGeometryExample

--- EtherGL-Examples/00_basic/ch/fhnw/ether/examples/basic/CustomGeometryExample.java
+++ EtherGL-Examples/00_basic/ch/fhnw/ether/examples/basic/CustomGeometryExample.java
@@ -54,6 +54,7 @@ public final class CustomGeometryExample {
 				off + 0, 0, 
 				off + 0, 0.5f, 
 				off + 0.5f, 0, 0.5f };
+		float[] colors = { 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1 };
 
 		DefaultGeometry g = DefaultGeometry.createVC(Primitive.TRIANGLES, vertices, colors);
 		return new DefaultMesh(new ColorMaterial(RGBA.WHITE, true), g, IMesh.Queue.TRANSPARENCY);
